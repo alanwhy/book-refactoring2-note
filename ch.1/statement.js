01a02342f5c65347257e399d6bebde94603706f1
wuhaoyuan <wuhaoyuan11101@navinfo.com>
2023-07-14 20:34:11 +0800

    新函数里的变量改名

--- ch.1/statement.js
+++ ch.1/statement.js
@@ -4,12 +4,12 @@ function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
 
-function volumeCreditsFor(perf) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
-  if ("comedy" === playFor(perf).type)
-    volumeCredits += Math.floor(perf.audience / 5);
-  return volumeCredits;
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+  return result;
 }
 
 // 为参数取名时都默认带上其类型名。
