aba9bd547f0a52ae17bf885dc858b602165de63f
wuhaoyuan <wuhaoyuan11101@navinfo.com>
2023-07-14 20:39:20 +0800

    rename format

--- ch.1/statement.js
+++ ch.1/statement.js
@@ -4,12 +4,12 @@ function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
 
-function format(aNumber) {
+function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
-  }).format(aNumber);
+  }).format(aNumber / 100);
 }
 
 function volumeCreditsFor(aPerformance) {
@@ -59,12 +59,12 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
