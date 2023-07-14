ad074d79f2ca7c75912b8178d9f6a0380df92377
wuhaoyuan <wuhaoyuan11101@navinfo.com>
2023-07-14 20:42:23 +0800

    更新 volumeCredits 变量相关的代码都集中到一起

--- ch.1/statement.js
+++ ch.1/statement.js
@@ -53,16 +53,18 @@ function amountFor(aPerformance, play) {
 
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
+  }
+  // 更新 volumeCredits 变量相关的代码都集中到一起，有利于以查询取代临时变量（178）手法的施展
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
