0c346f01f722085bc4351f9727bb036f249e8062
wuhaoyuan <wuhaoyuan11101@navinfo.com>
2023-07-17 13:08:19 +0800

    1.8.1

--- ch.1/createStatementData.js
+++ ch.1/createStatementData.js
@@ -1,12 +1,24 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+}
+
+// 创建演出计算器
 function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
@@ -39,6 +51,7 @@ function createStatementData(invoice, plays) {
     }
     return result;
   }
+
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
@@ -46,12 +59,13 @@ function createStatementData(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
+
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.amount, 0);
   }
+
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
-
 module.exports = createStatementData;
