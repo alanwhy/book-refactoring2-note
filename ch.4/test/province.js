0b39dc9c2a1e1ad3ce3e4606a70b38c2cb519f18
alanwhy <570028237@qq.com>
2023-08-13 15:39:27 +0800

    4.4

---
+++ ch.4/test/province.js
@@ -0,0 +1,34 @@
+import Province from "../src/Province.js";
+// import assert from "assert";
+import { expect } from "chai";
+
+function sampleProvinceData() {
+  return {
+    name: "Asia",
+    producers: [
+      { name: "Byzantium", cost: 10, production: 9 },
+      { name: "Attalia", cost: 12, production: 10 },
+      { name: "Sinope", cost: 10, production: 6 },
+    ],
+    demand: 30,
+    price: 20,
+  };
+}
+
+describe("province", function () {
+  it("shortfall", function () {
+    const asia = new Province(sampleProvinceData()); // DON'T DO THIS
+    expect(asia.shortfall).equal(5);
+  });
+  it("profit", function () {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.profit).equal(230);
+  });
+});
+
+// describe("province", function () {
+//   it("shortfall", function () {
+//     const asia = new Province(sampleProvinceData());
+//     assert.equal(asia.shortfall, 5);
+//   });
+// });
